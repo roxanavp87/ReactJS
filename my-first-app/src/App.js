a365eacd4d0a5aebfe788fb3f2f60f3a8ceb7b7b
roxanavp87 <roxanavp87@gmail.com>
2019-02-25 11:25:21 -0600

    example of using state and setState

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -4,43 +4,55 @@ import './App.css';
 import Table from './Table.js';
 
 class App extends Component {
-    render() {
-        const characters = [{
-                'name': 'Charlie',
-                'job': 'Janitor'
-            },
-            {
-                'name': 'Mac',
-                'job': 'Bouncer'
-            },
-            {
-                'name': 'Dee',
-                'job': 'Aspring actress'
-            },
-            {
-                'name': 'Dennis',
-                'job': 'Bartender'
-            }
-        ];
-        return ( < Table characterData = { characters } />
-            // <div className="App">
-            //   <header className="App-header">
-            //     <img src={logo} className="App-logo" alt="logo" />
-            //     <p>
-            //       Edit <code>src/App.js</code> and save to reload.
-            //     </p>
-            //     <a
-            //       className="App-link"
-            //       href="https://reactjs.org"
-            //       target="_blank"
-            //       rel="noopener noreferrer"
-            //     >
-            //       Learn React
-            //     </a>
-            //   </header>
-            // </div>
-        );
-    }
+  state = {
+    characters: [
+      {
+        'name': 'Charlie',
+        'job': 'Janitor'
+      },
+      {
+        'name': 'Mac',
+        'job': 'Bouncer'
+      },
+      {
+        'name': 'Dee',
+        'job': 'Aspring actress'
+      },
+      {
+        'name': 'Dennis',
+        'job': 'Bartender'
+      }
+    ]
+  }
+  removeCharacter = index => {
+    const {characters} = this.state;
+    this.setState({
+        characters: characters.filter((character, i) => {
+          return i !== index;
+        })
+    })
+  }
+
+  render() {
+      return ( < Table characterData = {this.state.characters} removeCharacter={this.removeCharacter} />
+          // <div className="App">
+          //   <header className="App-header">
+          //     <img src={logo} className="App-logo" alt="logo" />
+          //     <p>
+          //       Edit <code>src/App.js</code> and save to reload.
+          //     </p>
+          //     <a
+          //       className="App-link"
+          //       href="https://reactjs.org"
+          //       target="_blank"
+          //       rel="noopener noreferrer"
+          //     >
+          //       Learn React
+          //     </a>
+          //   </header>
+          // </div>
+      );
+  }
 }
 
 export default App;
--- my-first-app/src/Table.js
+++ my-first-app/src/Table.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const Table = ({ characterData}) => {
+const Table = ({ characterData, removeCharacter}) => {
     const rows = characterData.map( (row, index) => {
         return(
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
+                <td><button onClick={() => removeCharacter(index)}>Delete</button></td>
             </tr>
         );
     })
