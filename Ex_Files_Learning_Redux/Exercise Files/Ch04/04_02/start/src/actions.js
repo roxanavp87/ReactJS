c9adec732d79298a963447c827ac937f70aaa108
roxanavp87 <roxanavp87@gmail.com>
2019-03-05 22:04:14 -0600

    create actions

--- Ex_Files_Learning_Redux/Exercise Files/Ch04/04_02/start/src/actions.js
+++ Ex_Files_Learning_Redux/Exercise Files/Ch04/04_02/start/src/actions.js
@@ -1,4 +1,5 @@
 import C from './constants'
+import { suggestions } from './store/reducers';
 
 export function addDay(resort, date, powder=false, backcountry=false) {
 
@@ -24,5 +25,32 @@ export const setGoal = (goal) =>
 		payload: goal
 	})
 
+export const addError = error => {
+	return {
+		type: C.ADD_ERROR,
+		payload: error
+	}
+
+}
+
+export const clearError = index => {
+	return {
+		type: C.CLEAR_ERROR,
+		payload: index
+	}
+
+}
+
+export const changeSuggestions = suggestions => 
+({
+	type: C.CHANGE_SUGGESTIONS,
+	payload: suggestions
+})
 
 
+
+export const clearSuggestions = () =>
+	({
+		type: C.CLEAR_SUGGESTIONS,
+		payload: []
+	})
