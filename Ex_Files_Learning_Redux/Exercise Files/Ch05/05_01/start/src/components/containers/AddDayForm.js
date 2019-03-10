21b70b3b4d658a5357d60b18b432fd984d20d0f4
roxanavp87 <roxanavp87@gmail.com>
2019-03-10 17:11:09 -0500

    connect AddDayForm

--- Ex_Files_Learning_Redux/Exercise Files/Ch05/05_01/start/src/components/containers/AddDayForm.js
+++ Ex_Files_Learning_Redux/Exercise Files/Ch05/05_01/start/src/components/containers/AddDayForm.js
@@ -1,12 +1,38 @@
 import AddDayForm from '../ui/AddDayForm'
-import { withRouter } from 'react-router'
+import { withRouter } from 'react-router' 
+import { connect } from 'react-redux'
+import { addDay, suggestResortNames, clearSuggestions } from '../../actions'
 
-export default withRouter(
-    (props) => 
-        <AddDayForm suggestions={[]} 
-                fetching={false} 
-                router={props.router} 
-                onNewDay={day => console.log('todo: add day', day)}
-                onChange={value => console.log('todo: suggest', value)}
-                onClear={() => console.log('todo: clear suggestions')} />
-)
+const mapStateToProps = (state, props) => ({
+    suggestions: state.suggestions,
+    fetching: state.resortNames.fetching,
+    router: props.router
+})
+
+const mapDiscpatchToProps = dispatch => ({
+    onNewDay({ resort, date, powder, backcountry}) {
+        dispatch(
+            addDay(resort, date, powder, backcountry)
+        )
+    },
+    onChange(value) {
+        if (value) {
+            dispatch(
+                suggestResortNames(value)
+            )
+        } else {
+            dispatch(
+                clearSuggestions()
+            )
+        }
+    },
+    onClear() {
+        dispatch(
+            clearSuggestions()
+        )
+    }
+})
+
+const Container = connect(mapStateToProps, mapDiscpatchToProps)(AddDayForm);
+
+export default withRouter(Container);
