1042f2c7470f54c108e6227b651eca8d1a99edd4
roxanavp87 <roxanavp87@gmail.com>
2019-03-10 10:48:21 -0500

    npm i

--- Ex_Files_Learning_Redux/Exercise Files/Ch05/05_01/start/src/index.js
+++ Ex_Files_Learning_Redux/Exercise Files/Ch05/05_01/start/src/index.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import routes from './routes'
 import sampleData from './initialState'
+import storeFactory from './store'
+import {Provider} from 'react-redux'
 
 const initialState = (localStorage["redux-store"]) ?
     JSON.parse(localStorage["redux-store"]) :
@@ -10,10 +12,15 @@ const initialState = (localStorage["redux-store"]) ?
 
 const saveState = () => 
     localStorage["redux-store"] = JSON.stringify(store.getState())
+const store = storeFactory(initialState)
+store.subscribe(saveState)
 
 window.React = React
+window.store = store;
 
 render(
-	routes,
+  <Provider store={store}>
+	 {routes}
+  </Provider>,
   document.getElementById('react-container')
 )
